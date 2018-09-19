383aca820d83116b84d19f1fedf9113530200a54
lee <bo.leeing@gmail.com>
2018-09-19 11:07:51 +0800

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code React apps</h1>
+    <h1>Learn to design<br/> and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
